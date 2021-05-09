ea15c9ef4dcab3eee31f2986062aa3083d6ad6c1
DavideMottini8192 <77574818+DavideMottini8192@users.noreply.github.com>
2021-05-09 18:31:53 +0100

    Reset per prima modifica estesa
    Controllare l'upload seguente per i dettagli della modifica, c'è stata una parziale ristrutturazione e il programma inizia già a prendere un po' forma

--- PgAr2021_IlTriumvirato_TamaGolem/src/arnaldo/anno2021/triumvirato/tamagolem/Giocatore.java
+++
@@ -1,7 +0,0 @@
-package arnaldo.anno2021.triumvirato.tamagolem;
-
-public class Giocatore {
-	String nome;
-	Golem golem;
-	
-}
--- PgAr2021_IlTriumvirato_TamaGolem/src/arnaldo/anno2021/triumvirato/tamagolem/MappaElementale.java
+++
@@ -1,5 +0,0 @@
-package arnaldo.anno2021.triumvirato.tamagolem;
-
-public class MappaElementale {
-	
-}
